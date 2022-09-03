92ee68feedaf9ba95ba0b5a89cf69a0ac7affce5
Nastya <tibeikina@inbox.ru>
2022-09-03 18:23:19 +0600

    add Tast1

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int n = a / 10; 
+int m = n % 10; 
+
+Console.WriteLine("Вторая цифра этого числа - " + m);
