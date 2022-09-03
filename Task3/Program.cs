6eb4fdb331bfc2b0635961b680720ef33eb55e10
Nastya <tibeikina@inbox.ru>
2022-09-03 19:19:22 +0600

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите цифру от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (a > 7 || a < 1)
+{
+    Console.Write("Надеюсь, что это просто опечатка, попробуйте еще раз:  ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+if (a == 6 || a == 7)
+Console.Write("Этот день выходной");
+else
+Console.Write("Не время расслабляться");
