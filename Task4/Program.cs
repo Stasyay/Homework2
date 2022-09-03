326e3516925153af00f4837d0a3216dc76d8143c
Nastya <tibeikina@inbox.ru>
2022-09-03 21:15:38 +0600

    add Task4 Лесенка

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Лесенка 
+
+Console.Write("Введите цифру от 1 до 9: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (a > 9 || a < 1)
+{
+    Console.Write("Неправильно, попробуйте еще раз:  ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 1; i <= a; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(j);      
+    }  
+  Console.WriteLine("");
+}
