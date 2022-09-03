b320f7e1458e39c4c9a4c9f5251d10fe815ef52e
Nastya <tibeikina@inbox.ru>
2022-09-03 19:04:48 +0600

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 0) 
+a = a*-1;
+
+if (a < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+        while (a >= 1000)
+        a = a/10; 
+}
+ a = a%10;
+Console.WriteLine(a);
