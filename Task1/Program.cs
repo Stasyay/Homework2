419b2ca5a58d6241e2b579edcf5e3062c8e5fd20
Nastya <tibeikina@inbox.ru>
2022-09-03 19:27:19 +0600

    add change Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,11 @@
 ﻿Console.Write("Введите трехзначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-
+while (a > 999 || a < 100)
+{
+    Console.Write("Нужно ввести трехзначное :  ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
 int n = a / 10; 
 int m = n % 10; 
 
-Console.WriteLine("Вторая цифра этого числа - " + m);
+Console.WriteLine("Вторая цифра этого числа: " + m);
