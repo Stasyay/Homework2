491a519f47b110c5886418a8208f3164a97cfb9a
Nastya <tibeikina@inbox.ru>
2022-09-04 14:00:03 +0600

    add Task5 Мороженое

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Мороженое
+Console.WriteLine("Введите целое, положительное число: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+while (k < 0)
+{
+    Console.Write("Неправильно, попробуйте еще раз:  ");
+    k = Convert.ToInt32(Console.ReadLine());
+}
+
+if (k % 3 == 0 || k % 5 ==0 || k % 8 ==0)
+Console.WriteLine("yes");
+else
+Console.WriteLine("No");
+
